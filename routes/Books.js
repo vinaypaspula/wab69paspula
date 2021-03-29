8b73e478dc957fd681a91f6f85c3dfe3a9e99e08
Vinay Kumar Paspula <vinaykmr149@gmail.com>
2021-03-28 20:23:44 -0500

    Added endpoint link to the index

--- routes/Books.js
+++ routes/Books.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Books', { title: 'Search Results Books' });
+  res.render('Books', { title: 'Search Results Books ' });
 });
 
 module.exports = router;
