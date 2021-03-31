065694a45bfbd91a1e8a1d0466e70a33486f8cfc
Vinay Kumar Paspula <vinaykmr149@gmail.com>
2021-03-31 00:34:38 -0500

    Pulled images

--- routes/slot.js
+++ routes/slot.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Slot Machine' });
+  res.render('slot', { title: 'Slot Machine' });
 });
 
 module.exports = router;
